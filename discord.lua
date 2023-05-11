1096c823b9bb82b11c57aa3efa9090befee78247
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2023-05-11 14:32:11 -0700

    Improves error handling in sendToDiscordWebhook

--- discord.lua
+++ discord.lua
@@ -2,6 +2,7 @@ require("common")
 local http = require("socket.http")
 local https = require("socket.ssl.https")
 local json = require("json")
+local chat = require('chat')
 
 function sendToDiscordWebhook(message, webhook_url, avatar_url)
     local payload = {
@@ -25,18 +26,7 @@ function sendToDiscordWebhook(message, webhook_url, avatar_url)
         sink = ltn12.sink.table(response_body)
     }
 
-    --[[
-        TODO: This is for debugging failed requests
-        In regular operation we just fire the webhook and
-        hope for the best. But in the future maybe some
-        error notification to the user would be good
-        so they can post the TOD themselves. At least
-        this would be able to print out an accurate
-        timestamp for them
-        TODO: Make this a function that prints this out
-        if there is an error. Along with the timestamp.
-    ]]
-    -- local response_str = table.concat(response_body)
-    -- print(response_status)
-    -- print(response_str)
+    local response_str = table.concat(response_body)
+    print(chat.header('todbot') .. chat.error("Unable to deliver message " .. message))
+    print(chat.header('todbot') .. chat.error(response_str))
 end
