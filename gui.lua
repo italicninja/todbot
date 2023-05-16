d6c43b811599c30e583ee0addda0e32e7923436e
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2023-05-16 15:18:37 -0700

    Adds table layout for popup and adds timer since killed

--- gui.lua
+++ gui.lua
@@ -50,27 +50,53 @@ function render_monster_tod_popup(todbot)
 
     local recent_monsters = deepCopy(todbot.recent_monsters)
     imgui.SetNextWindowSize({ -1, -1 })
-    if (imgui.Begin('monster_tod', monster_tod_open)) then
-        for i, monster in ipairs(recent_monsters) do
-            imgui.Text(monster.name)
-            imgui.SameLine()
-            local pt_format = "%Y-%m-%d %H:%M:%S %Z"
-            local pt_timestamp = os.date(pt_format, monster.timestamp)
-            imgui.Text(pt_timestamp)
-            imgui.SameLine()
-            if( imgui.Button("Post") ) then
-                local message = string.format("%s: <t:%d:T> <t:%d:R> ", monster.name, monster.timestamp, monster.timestamp)
-                ashita.tasks.once(0, function()
-                    sendToDiscordWebhook(message, todbot.settings.webhookURL, todbot.settings.avatarURL)
-                end)
-                table.remove(todbot.recent_monsters, i)
-            end
-            imgui.SameLine()
-            if( imgui.Button("Dismiss") ) then
-                table.remove(todbot.recent_monsters, i)
-            end
-            imgui.Separator()
+    if (imgui.Begin('monster_tod', monster_tod_open, ImGuiWindowFlags_NoCollapse)) then
+        if( imgui.Button("Dismiss All") ) then
+            todbot.recent_monsters = T{}
         end
+        imgui.Separator()
+
+        if imgui.BeginTable('##tods', 4, bit.bor(ImGuiTableFlags_RowBg, ImGuiTableFlags_BordersH, ImGuiTableFlags_BordersV, ImGuiTableFlags_ScrollY)) then
+            imgui.TableSetupColumn('Name', ImGuiTableColumnFlags_WidthStretch, 0, 0)
+            imgui.TableSetupColumn('TOD', ImGuiTableColumnFlags_WidthFixed, 300, 0)
+            imgui.TableSetupColumn('Since', ImGuiTableColumnFlags_WidthFixed, 50, 0)
+            imgui.TableSetupColumn('Action', ImGuiTableColumnFlags_WidthFixed, 100, 0)
+            imgui.TableHeadersRow();
+
+            for i, monster in ipairs(recent_monsters) do
+                imgui.PushID(i)
+                imgui.TableNextRow()
+                imgui.TableSetColumnIndex(0)
+
+                imgui.Text(monster.name)
+                imgui.TableNextColumn()
+
+                local pt_format = "%Y-%m-%d %H:%M:%S %Z"
+                local pt_timestamp = os.date(pt_format, monster.timestamp)
+                imgui.Text(pt_timestamp)
+                imgui.TableNextColumn()
+
+                local since_format = "%M:%S"
+                local since = os.date(since_format, os.time() - monster.timestamp)
+                imgui.Text(since)
+                imgui.TableNextColumn()
+
+                if( imgui.Button("Post") ) then
+                    local message = string.format("%s: <t:%d:T> <t:%d:R> ", monster.name, monster.timestamp, monster.timestamp)
+                    ashita.tasks.once(0, function()
+                        sendToDiscordWebhook(message, todbot.settings.webhookURL, todbot.settings.avatarURL)
+                    end)
+                    table.remove(todbot.recent_monsters, i)
+                end
+                imgui.SameLine()
+                if( imgui.Button("Dismiss") ) then
+                    table.remove(todbot.recent_monsters, i)
+                end
+
+                imgui.PopID()
+            end
+        end
+        imgui.EndTable()
     end
     imgui.End()
 end
